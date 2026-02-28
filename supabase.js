3e24c311b744c3a79cc1a1a54fd99dd94152297e
adrianolegal <adrianotni2017@gmail.com>
2026-02-28 14:26:16 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,25 +1,22 @@
 // supabase.js
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/supabase.min.js";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.38.0/dist/supabase.js";
 
 const SUPABASE_URL = "https://qgsfmiywdohybqlnxhdw.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnc2ZtaXl3ZG9oeWJxbG54aGR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyNzg3MzAsImV4cCI6MjA4Nzg1NDczMH0.Ku7lQF-YzwyPqm_igXk3tYnfgmSalzsHNmmVBP9NCp0";
+const SUPABASE_ANON_KEY = "SUA_ANON_KEY_AQUI"; // coloque anon key apenas aqui
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-// Login
 export async function login(email, password) {
   const { data, error } = await supabaseClient.auth.signInWithPassword({ email, password });
   if (error) throw error;
   return data.session;
 }
 
-// Check session
 export async function checkAuth() {
   const { data } = await supabaseClient.auth.getSession();
   return data.session;
 }
 
-// Logout
 export async function logout() {
   await supabaseClient.auth.signOut();
 }
