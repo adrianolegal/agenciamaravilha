131191104af2eb3083398089aaddff7be6eb3f8b
adrianolegal <adrianotni2017@gmail.com>
2026-02-28 14:27:31 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.38.0/dist/supabase.js";
 
 const SUPABASE_URL = "https://qgsfmiywdohybqlnxhdw.supabase.co";
-const SUPABASE_ANON_KEY = "SUA_ANON_KEY_AQUI"; // coloque anon key apenas aqui
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnc2ZtaXl3ZG9oeWJxbG54aGR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyNzg3MzAsImV4cCI6MjA4Nzg1NDczMH0.Ku7lQF-YzwyPqm_igXk3tYnfgmSalzsHNmmVBP9NCp0"; // coloque anon key apenas aqui
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
